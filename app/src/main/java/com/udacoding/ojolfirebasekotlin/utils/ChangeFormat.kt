fcdd63b622f425fb3252bf97f1eb0fb6a7f095d0
Bas Bas <basriumar12@gmail.com>
2019-10-02 09:52:38 +0700

    date utils

--- app/src/main/java/com/udacoding/ojolfirebasekotlin/utils/ChangeFormat.kt
+++ app/src/main/java/com/udacoding/ojolfirebasekotlin/utils/ChangeFormat.kt
@@ -38,3 +38,32 @@ object ChangeFormat {
 }
 
 
+/// date utils bang nando
+
+object DateUtils {
+    const val DATE_FORMAT_YYYY = "yyyy-MM-dd"
+    const val DATE_FORMAT_FULL = "EEEE dd MMM yyyy"
+    const val DATE_FORMAT_HISTORY = "dd MMMM yyyy"
+
+    fun formatStringToDate(date: String, format: String) : Date{
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        return sdf.parse(date)
+    }
+
+    fun formatDateLong(date: Long, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale.getDefault())
+        return sdfDest.format(Date(date))
+    }
+
+    fun formatDateLongLocale(date: Long, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale("in","ID"))
+        return sdfDest.format(Date(date))
+    }
+    fun formatDateLocale(date: Date, destinationFormat: String) : String{
+        val sdfDest = SimpleDateFormat(destinationFormat, Locale("in","ID"))
+        return sdfDest.format(date)
+    }
+
+}
+
+
